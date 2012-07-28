6b22ffd943f856f8640f12927e8c543721f5ec2a
Matthew <makaimc@gmail.com>
2012-07-28 11:26:41 -0400

    Adding settings.py file and updating with a readme file.

---
+++ README.md
@@ -0,0 +1,2 @@
+Source code for `my technical blog <http://www.mattmakai.com/>`, hosted
+by Github Pages.
---
+++ source/settings.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+AUTHOR = u'Matt Makai'
+SITENAME = u'Matt Makai'
+SITEURL = 'http://www.mattmakai.com'
+TIMEZONE = 'America/New_York'
+
+GITHUB_URL = 'https://github.com/makaimc/mattmakai.github.com'
+DISQUS_SITENAME = 'makaimc'
+PDF_GENERATOR = False
+
+BYLINE = '&copy; 2012 Matt Makai. All Rights Reserved.'
+LINKS = (
+    ('Presentations', '/pages/presentations.html'),
+)
+
+SOCIAL = (
+    ('Email', 'mailto:makaimc@gmail.com'),
+    ('GitHub', 'https://github.com/makaimc'),
+    ('Twitter', 'http://twitter.com/makaimc'),
+)
+
+PROJECTS = (
+    ('ProofDriven', 'http://www.proofdriven.com/',
+    'Upgrade Urgency Financial Reports for Software Developers'),
+)
+
+JINJA_EXTENSIONS = (['jinja2.ext.autoescape',])
+
